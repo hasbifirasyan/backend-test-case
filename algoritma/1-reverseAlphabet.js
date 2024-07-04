a3fdf8c2d3ab5e2cd6f9c3fd12d87b597317a3f3
Hasbi Firasyan <firasyanhasbi@gmail.com>
2024-07-04 08:25:40 +0700

    algoritma 1 reverse done

---
+++ algoritma/1-reverseAlphabet.js
@@ -0,0 +1,11 @@
+// 1. Terdapat string "NEGIE1", silahkan reverse alphabet nya dengan angka tetap diakhir kata Hasil = "EIGEN1"
+function alphabetReverser(str) {
+  const num = str[str.length - 1];
+  let result = "";
+  for (let i = str.length - 2; i >= 0; i--) {
+    result += str[i];
+  }
+  return result + num;
+}
+
+console.log(alphabetReverser(`NEGIE1`));
